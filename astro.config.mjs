616365671b50804dd933f50cc2bc3d38df912a96
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-10-14 20:46:18 -0600

    formmated and removed previous code

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://binarybrains-upiicsa.github.io",
-  base: "BrainHack",
+  base: "LogisticsHackathon",
 
   vite: {
     plugins: [tailwindcss()],
