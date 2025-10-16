da19430be923c214f1b103fb1d37ad57eab4bd89
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-10-16 07:50:30 -0600

    background and countdown redesign

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,12 +5,14 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils.ts";
 
 const buttonVariants = cva(
-    "inline-flex items-center justify-center gap-4 whitespace-nowrap rounded-full w-[185px] h-[41px] font-poppins text-sm font-semibold transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-4 whitespace-nowrap rounded-full w-[185px] h-[41px] font-poppins text-sm font-semibold transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
         primary: "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
         secondary: "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/90",
+        translucent:
+          "bg-[rgba(255,255,255,0.08)] hover:bg-[rgba(255,255,255,0.64)] hover:text-[rgb(19,21,23)] transition-all duration-300 ease-[cubic-bezier(.4,0,.2,1)]",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
@@ -24,7 +26,7 @@ const buttonVariants = cva(
       size: "default",
     },
   },
-  );
+);
 
 function Button({
   className,
