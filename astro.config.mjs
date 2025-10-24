f2e5a4bc595285062f4cc281f9b8dac8a5723f5f
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2025-10-24 14:45:16 -0600

    feat: Fixed responsive design of communities and navbar (#64)
    * feat: Fixed responsive design of communities and navbar
    
    * style

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,4 +15,7 @@ export default defineConfig({
   },
 
   integrations: [react()],
+  devToolbar: {
+    enabled: false,
+  },
 });
---
+++ src/data/communities.ts
@@ -0,0 +1,33 @@
+export type Community = {
+  src: string;
+  alt: string;
+  height: number;
+  width: number;
+};
+
+export const COMMUNITIES: Community[] = [
+  {
+    alt: "ASCM",
+    src: "/LogisticsHackathon/communities/ASCM_LOGOS_STUDENTFORUM_COLOR_W_UPIICSA.png",
+    width: 200,
+    height: 100,
+  },
+  {
+    alt: "Binary Brains",
+    src: "/LogisticsHackathon/communities/image.png",
+    width: 180,
+    height: 90,
+  },
+  {
+    alt: "Github",
+    src: "/LogisticsHackathon/communities/github.png",
+    width: 180,
+    height: 90,
+  },
+  {
+    alt: "Notion",
+    src: "/LogisticsHackathon/communities/Notion_app_logo.png",
+    width: 180,
+    height: 90,
+  },
+];
