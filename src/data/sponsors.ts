3abd6b3b0da06083a7fd62555f208bdde0baf824
Arturo Isaí Casco Serrano <102835392+casco613@users.noreply.github.com>
2025-10-26 17:52:55 -0600

    feat: se implementa el apartado de sponsors junto con sus imagenes png (#66)
    * feat: se implementa el apartado de sponsors junto con sus imagenes png
    
    * feat: dino format
    
    * feat: ya quedo
    
    ---------
    
    Co-authored-by: casco613 <casco613@MacBook-Pro-de-Arturo.local>

---
+++ src/data/sponsors.ts
@@ -0,0 +1,33 @@
+export type Sponsors = {
+  src: string;
+  alt: string;
+  height: number;
+  width: number;
+};
+
+export const SPONSORS: Sponsors[] = [
+  {
+    alt: "EDSM",
+    src: "/LogisticsHackathon/sponsors/EDSM.PNG",
+    width: 200,
+    height: 100.,
+  },
+  {
+    alt: "IBM",
+    src: "/LogisticsHackathon/sponsors/IBM.png",
+    width: 200,
+    height: 100.,
+  },
+  {
+    alt: "LDM",
+    src: "/LogisticsHackathon/sponsors/LDM.png",
+    width: 200,
+    height: 100.,
+  },
+  {
+    alt: "JP",
+    src: "/LogisticsHackathon/sponsors/PJ.png",
+    width: 200,
+    height: 100.,
+  },
+];
