37bfac5e86bd5348391d01c154fc549782ca8472
bryancsinformatica <bryancsinformatica@gmail.com>
2025-10-27 19:40:04 -0600

    feat: add Mr faccio+

--- src/data/mentors.ts
+++ src/data/mentors.ts
@@ -49,7 +49,7 @@ export const MENTORS: Mentor[] = [
     role: "AI Engineer @ IBM",
     profilePath: "/LogisticsHackathon/mentors/erandi.jpg",
   },
-    {
+  {
     name: "Manuel Ortiz",
     technologies: [
       "Azure",
@@ -61,5 +61,15 @@ export const MENTORS: Mentor[] = [
     ],
     role: "Microsoft DevOps MVP | Cloud & Software Engineer",
     profilePath: "/LogisticsHackathon/mentors/manu.jpeg",
+  },
+  {
+    name: "Lenin Facio (Mr. Faccio)",
+    technologies: [
+      "Pentesting web",
+      "OSINT",
+      "Hacking",
+    ],
+    role: "Pentester at Haak Cybersecurity Consulting | Co-founder @cybertech_comu",
+    profilePath: "/LogisticsHackathon/mentors/lenin.jpeg",
   },
 ];
