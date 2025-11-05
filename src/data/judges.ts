a73dacca1144cb048864692229e61bc4e5639d4a
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-11-04 22:04:20 -0600

    added judges from LDM

--- src/data/judges.ts
+++ src/data/judges.ts
@@ -7,10 +7,10 @@ type Judge = {
 
 export const JUDGES: Judge[] = [
   {
-    name: "José Jesús Guzmán",
-    role: "Cloud Arquitect at Caylent, AWS Community Builder",
+    name: "José Jesús Guzmán (BrujeriaTech)",
+    role: "Cloud and AI specialist | Ex-Microsoft | Backend developer | AI and Data Science expert",
     profilePath: "brujeria.png",
-    className: "to-zinc-950",
+    className: "to-zinc-950/80",
   },
   {
     name: "David Sol",
@@ -22,13 +22,37 @@ export const JUDGES: Judge[] = [
     name: "Susana Cuevas",
     role: "Jefa del Programa Académico de Ingeniería en Informática - UPIICSA IPN",
     profilePath: "susana-cuevas.png",
-    className: "to-zinc-950",
+    className: "to-zinc-950/80",
   },
   {
     name: "Eduardo Dominguez (EduDom)",
     role:
       "Delivery Consultant Data & AI and Sustainability @ IBM | AI Lead & Co-founder @ BinaryBrains",
     profilePath: "edu-dom.png",
-    className: "to-zinc-950",
+    className: "to-zinc-950/80",
+  },
+  {
+    name: "Jorge Evaristo Pardo Ruíz",
+    role: "Consultor Sr. @ LDM",
+    profilePath: "jorge.png",
+    className: "to-gray-800/70",
+  },
+  {
+    name: "José Arturo Gómez Arrucha",
+    role: "Gerente de Proyectos @ LDM",
+    profilePath: "jose_arturo_gomez.png",
+    className: "to-stone-300/40",
+  },
+  {
+    name: "Jocelyn Adonay Cerna Camacho ",
+    role: "Consutlor @ LDM",
+    profilePath: "jocelyn_adonay.png",
+    className: "to-stone-300/40",
+  },
+  {
+    name: "Iván Guerrero",
+    role: "Subdirector de Operaciones @ LDM",
+    profilePath: "LDM.png",
+    className: "",
   },
 ];
