366b0c78b2282c7209d256266f089caeb5577bb1
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2025-11-04 22:13:16 -0600

    feat: Created resource section (#94)
    * feat: Created resources page
    
    * added judges from LDM
    
    * added judges

--- src/data/judges.ts
+++ src/data/judges.ts
@@ -7,21 +7,52 @@ type Judge = {
 
 export const JUDGES: Judge[] = [
   {
-    name: "José Jesús Guzmán",
-    role: "Cloud Arquitect at Caylent, AWS Community Builder",
+    name: "José Jesús Guzmán (BrujeriaTech)",
+    role: "Cloud and AI specialist | Ex-Microsoft | Backend developer | AI and Data Science expert",
     profilePath: "brujeria.png",
-    className: "to-slate-950",
+    className: "to-zinc-950/80",
   },
   {
     name: "David Sol",
     role: "Cloud Arquitect at Caylent, AWS Community Builder",
     profilePath: "david-sol.png",
-    className: "to-slate-400",
+    className: "to-slate-500",
   },
   {
     name: "Susana Cuevas",
     role: "Jefa del Programa Académico de Ingeniería en Informática - UPIICSA IPN",
     profilePath: "susana-cuevas.png",
-    className: "to-slate-950",
+    className: "to-zinc-950/80",
+  },
+  {
+    name: "Eduardo Dominguez (EduDom)",
+    role:
+      "Delivery Consultant Data & AI and Sustainability @ IBM | AI Lead & Co-founder @ BinaryBrains",
+    profilePath: "edu-dom.png",
+    className: "to-zinc-950/80",
+  },
+  {
+    name: "Jorge Evaristo Pardo Ruíz",
+    role: "Consultor Sr. @ LDM",
+    profilePath: "jorge.png",
+    className: "to-gray-800/70",
+  },
+  {
+    name: "José Arturo Gómez Arrucha",
+    role: "Gerente de Proyectos @ LDM",
+    profilePath: "jose_arturo_gomez.png",
+    className: "to-stone-300/40",
+  },
+  {
+    name: "Jocelyn Adonay Cerna Camacho ",
+    role: "Consutlor @ LDM",
+    profilePath: "jocelyn_adonay.png",
+    className: "to-stone-300/30",
+  },
+  {
+    name: "Iván Guerrero",
+    role: "Subdirector de Operaciones @ LDM",
+    profilePath: "LDM.png",
+    className: "",
   },
 ];
---
+++ src/data/resources.ts
@@ -0,0 +1,48 @@
+export type Resource = {
+  company: {
+    logoPath: string;
+    name: string;
+  };
+  title: string;
+  description?: string;
+  links: { href: string; label: string }[];
+};
+export const RESOURCES: Resource[] = [
+  {
+    company: {
+      name: "IBM",
+      logoPath: "IBM_logo.png",
+    },
+    title: "Learning Path: AI - IBM Skills Build",
+    description:
+      "En este apartado encontrará cómo, como estudiante, obtener créditos en IBM Cloud para usar sus IA, dentro de un learning‑path que también incluye cursos adicionales, como desarrollo web.",
+
+    links: [
+      {
+        href: "https://ibm.biz/BdbSEA",
+        label:
+          "UPIICSA AI Summit: Aprende de los datos rápido y sin programar usando IA y reclama una credencial digital.",
+      },
+      {
+        href: "https://skills.yourlearning.ibm.com/channel/CNL_LCB_1569538212365",
+        label: "Communication Skills",
+      },
+    ],
+  },
+  {
+    company: {
+      name: "IBM",
+      logoPath: "IBM_logo.png",
+    },
+    title: "Learning Path:  JavaScript Web Development -IBM Skills Build",
+    description:
+      "En este apartado, dirigido a estudiantes, encontrará el learning‑path “Web Development: Learn JavaScript”, que reúne todos los recursos y ejercicios necesarios para dominar JavaScript desde cero y avanzar paso a paso en el desarrollo web.",
+
+    links: [
+      {
+        href: "https://skills.yourlearning.ibm.com/channel/CNL_LCB_1616532512410",
+        label: "Web Development: Learn JavaScript",
+      },
+    ],
+  },
+];
