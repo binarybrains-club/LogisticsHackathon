c82ee3b9288a44bf51e7f1900776ff3be304946d
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-11-04 21:11:27 -0600

    feat: Created resources page

--- src/data/judges.ts
+++ src/data/judges.ts
@@ -10,18 +10,25 @@ export const JUDGES: Judge[] = [
     name: "José Jesús Guzmán",
     role: "Cloud Arquitect at Caylent, AWS Community Builder",
     profilePath: "brujeria.png",
-    className: "to-slate-950",
+    className: "to-zinc-950",
   },
   {
     name: "David Sol",
     role: "Cloud Arquitect at Caylent, AWS Community Builder",
     profilePath: "david-sol.png",
-    className: "to-slate-400",
+    className: "to-slate-500",
   },
   {
     name: "Susana Cuevas",
     role: "Jefa del Programa Académico de Ingeniería en Informática - UPIICSA IPN",
     profilePath: "susana-cuevas.png",
-    className: "to-slate-950",
+    className: "to-zinc-950",
+  },
+  {
+    name: "Eduardo Dominguez (EduDom)",
+    role:
+      "Delivery Consultant Data & AI and Sustainability @ IBM | AI Lead & Co-founder @ BinaryBrains",
+    profilePath: "edu-dom.png",
+    className: "to-zinc-950",
   },
 ];
